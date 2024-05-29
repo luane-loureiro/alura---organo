e0468d174d45609813fae6c7b8b745e7dbb0f000
luane-loureiro <luachan@gmail.com>
2024-05-29 16:47:31 -0300

    update components

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,17 +5,6 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -57,8 +46,8 @@ const Formulario = (props) => {
                 />
                 <ListaSuspensa
                     obrigatorio={true}
-                    label="Time" 
-                    itens={times}
+                    label="Time"
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
