3f8065886d9b43e5c264c149c5c12e98248085af
luane-loureiro <luachan@gmail.com>
2024-05-29 16:58:05 -0300

    update components

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,10 +1,11 @@
 import './Colaborador.css'
 
-const Colaborador = ({ nome, imagem, cargo }) => {
+const Colaborador = ({nome, imagem, cargo, corDeFundo }) => {
+
     return (<div className='colaborador'>
-        <div className='cabecalho'>
+        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}> 
             <img src={imagem} alt={nome}/>
-        </div>
+        </div> 
         <div className='rodape'>
             <h4>{nome}</h4>
             <h5>{cargo}</h5>
