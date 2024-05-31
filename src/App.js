e66562189964f60867e9d9b3b711327f51b90f5e
luane-loureiro <luachan@gmail.com>
2024-05-31 14:11:53 -0300

    update bugs e cor do banner

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -60,7 +59,8 @@ function App() {
         nome={time.nome} 
         corPrimaria={time.corPrimaria} 
         corSecundaria={time.corSecundaria} 
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        colaboradores={colaboradores.filter(
+          colaborador => colaborador.time === time.nome)}
       />)}   
 
     </div>
