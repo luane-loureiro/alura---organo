ea3ba738ea77c1cc45fdae3e1517ca16a8da1a25
luane-loureiro <luachan@gmail.com>
2024-05-31 14:20:50 -0300

    updade banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,9 +3,12 @@ import './Banner.css'
 
 export const Banner = () => {
     return(
-        <header className = "banner">
-             <img src="/image/banner.png" alt="Banner principal da página do Organo"/>
-        </header>
+        <div>
+            <header className = "banner">
+                <img src="/image/banner.png" alt="Banner principal da página do Organo"/>
+            </header>
+        </div>
+
            
     )
 }
